971ec8d8661e09126a6b7721feb21eb05103fe4e
CoolRobots <darppdudee@gmail.com>
2024-12-10 16:51:01 -0600

    MEEP MEEP STUFF
    SO COOL

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -8,21 +9,21 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MyClass {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(90));
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(9, -60, Math.toRadians(90)))
+                .splineTo(new Vector2d(0, -33), Math.toRadians(90))
+                .strafeTo(new Vector2d(36, -33))
+                .strafeTo(new Vector2d(36, -10))
+                .strafeTo(new Vector2d(48, -10))
+                .strafeTo(new Vector2d(48, -52))
+                .strafeTo(new Vector2d(48, -10))
+                .strafeTo(new Vector2d(48, -10))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
